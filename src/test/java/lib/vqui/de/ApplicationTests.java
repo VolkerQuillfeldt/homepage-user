df531ffa3568d3192886d5d6ed4ade5d6b24e443
Volker Quillfeldt <Volker.Quillfeldt@gmail.com>
2020-03-22 14:55:14 +0100

    @ActiveProfiles(profiles ="dev")

--- src/test/java/lib/vqui/de/ApplicationTests.java
+++ src/test/java/lib/vqui/de/ApplicationTests.java
@@ -2,8 +2,10 @@ package lib.vqui.de;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles(profiles ="dev")
 class ApplicationTests {
 
 	@Test
